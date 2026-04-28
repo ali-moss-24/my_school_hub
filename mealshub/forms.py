a1359d050a8bb3e8a623d287467965c34e2b718b
ali-moss-24 <nodmi6n2@students.codeinstitute.net>
2026-04-28 19:45:42 +0100

    Fix OrderForm widget and indentation

--- mealshub/forms.py
+++ mealshub/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Order
 
-class OrderForm(forms.Model.Form):
-    model = Order
-    fields = ['student', 'date', 'notes']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['student', 'date', 'notes']
 
     widgets = {
-        'date': forms.DataInput(attrs={'type': 'date'}),
+        'date': forms.DateInput(attrs={'type': 'date'}),
         'notes': forms.Textarea(attrs={'rows': 3}),
     }
