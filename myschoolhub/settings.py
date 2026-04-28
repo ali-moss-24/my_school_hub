1c067d90c7e45328ec98f1f1787a92bbdbaeb506
ali-moss-24 <nodmi6n2@students.codeinstitute.net>
2026-04-28 21:19:59 +0100

    Add ordering app and templates directory

--- myschoolhub/settings.py
+++ myschoolhub/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mealshub',
+    'ordering',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'myschoolhub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- myschoolhub/urls.py
+++ myschoolhub/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mealshub.urls')),
+    path('ordering/', include('ordering.url')),
     ]
